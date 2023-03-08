3e513a262de8f3592c086b1745084d6683fecf13
ilya <zapaska96@gmail.com>
2023-03-08 18:11:18 +0500

    nav

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavBar } from './components/NavBar/NavBar';
+
+const App = () => {
+    return (
+        <div className='min-h-[200vh]'>
+            <NavBar />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const LINKS = ['music', 'sfx', 'color', 'vfx', 'log in', 'join'];
+
+export const NavBar = () => {
+    const [isHidden, setIsHidden] = useState(true);
+    const navRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!navRef.current) return;
+
+        const scrollFunc = () => {
+            if (!isHidden) return;
+            navRef.current?.classList.toggle('scrolled', window.scrollY > 0);
+        };
+
+        window.addEventListener('scroll', scrollFunc);
+
+        return () => {
+            window.removeEventListener('scroll', scrollFunc);
+        };
+    }, [isHidden]);
+
+    return (
+        <div className='sticky top-0 transition-all' ref={navRef}>
+            <div className='mx-auto max-w-[1450px]'>
+                <div className='relative flex min-h-[80px] justify-center p-6 lg:justify-between'>
+                    <div
+                        className={`absolute top-6 left-6 block cursor-pointer lg:hidden ${
+                            isHidden ? 'block' : 'hidden'
+                        }`}
+                        onClick={() => setIsHidden(false)}
+                    >
+                        X
+                    </div>
+                    <div
+                        className={`absolute left-0 top-5 h-screen w-4/5 bg-white p-6 ${
+                            isHidden ? 'hidden' : 'block'
+                        }`}
+                    >
+                        <ul className='flex flex-col gap-4'>
+                            {LINKS.map((link, index) => (
+                                <li
+                                    key={index}
+                                    className='cursor-pointer border-b p-2 text-base font-normal uppercase text-gray-700 hover:text-gray-500'
+                                >
+                                    {link}
+                                </li>
+                            ))}
+                        </ul>
+                        <div
+                            className='absolute right-2 top-2 cursor-pointer'
+                            onClick={() => setIsHidden(true)}
+                        >
+                            X
+                        </div>
+                    </div>
+                    <div className='flex items-center'>
+                        <div className='cursor-pointer uppercase'>
+                            Заголовок
+                        </div>
+                    </div>
+                    <div className='hidden items-center lg:flex'>
+                        <ul className='flex justify-center gap-2'>
+                            {LINKS.map((link, index) => (
+                                <li
+                                    key={index}
+                                    className='cursor-pointer text-base font-normal uppercase tracking-wide'
+                                >
+                                    {link}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
